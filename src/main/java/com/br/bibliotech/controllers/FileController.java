bd6457de26c2642bd984d75222e0a33a7e6d2828
davi gerson <dgas1@discente.ifpe.edu.br>
2025-06-09 20:31:16 -0300

    Estruturando o controller responsavel pelo upload e download de arquivos

--- src/main/java/com/br/bibliotech/controllers/FileController.java
+++ src/main/java/com/br/bibliotech/controllers/FileController.java
@@ -8,9 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
@@ -23,9 +26,22 @@ public class FileController implements FileControllerDocs {
     @Autowired
     private FileStorageService service;
 
+    @PostMapping("/uploadFile")
     @Override
-    public UploadFileResponseDTO uploadFile(MultipartFile file) {
-        return null;
+    public UploadFileResponseDTO uploadFile(@RequestParam("file") MultipartFile file) {
+        var filename = service.storeFile(file);
+
+        //http://localhost:8080/api/file/v1/donwloadFile/filename.docx
+        var fileDonwloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/donwloadFile/")
+                .path(filename)
+                .toUriString();
+
+        return new UploadFileResponseDTO(
+                filename,
+                fileDonwloadUri,
+                file.getContentType(),
+                file.getSize());
     }
 
     @Override
